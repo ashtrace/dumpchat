0332782dd79059e4abb5d2957e115e8a3c506144
ashtrace <0aishwaryaraj@gmail.com>
2022-02-25 18:12:05 +0530

    Added username functionality

--- client.c
+++ client.c
@@ -3,68 +3,99 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
-#define TRUE		1
-#define ADDR_SIZE	16
-#define MSG_SIZE	256
+#define TRUE			1
+#define ADDR_SIZE		16
+#define MSG_SIZE		256
+#define USR_NAME_SIZE	32
 
-void communicate(SOCKET);
+void recv_banner(SOCKET server_fd);
+void send_username(SOCKET server_fd, char *username);
+void communicate(SOCKET server_fd);
 
 int main(int argc, char *argv[]) {
 	WSADATA wsa;
 	SOCKET socket_fd;
 	struct sockaddr_in server;
 	char address[ADDR_SIZE];
+	char username[USR_NAME_SIZE];
 	unsigned int port = 0;
 
-	puts("[*] Initialising Winsock...\n");
+	puts("[DEBUG] Initialising Winsock...\n");
 
 	if (WSAStartup(MAKEWORD(2, 2), &wsa)) {
-		fprintf(stderr, "[!!] Failed to initialze Winsock. Error code: %d\n", WSAGetLastError());
+		fprintf(stderr, "[ERROR] Failed to initialze Winsock. Error code: %d\n", WSAGetLastError());
 		return -1;
 	}
 
-	puts("[*] Winsock Initialisation complete.\n");
+	puts("[DEBUG] Winsock Initialisation complete.\n");
 
 	if ( ( socket_fd = socket(AF_INET, SOCK_STREAM, 0) ) == INVALID_SOCKET ) {
-		fprintf(stderr, "[!!] Failed to create socket. Error Code: %d\n", WSAGetLastError());
+		fprintf(stderr, "[ERROR] Failed to create socket. Error Code: %d\n", WSAGetLastError());
 		return -1;
 	}
 
-	puts("[*] Socket Created.\n");
+	puts("[DEBUG] Socket Created.\n");
 
 	printf("[+] Enter server address: ");
 	scanf("%s", address);
 	printf("[+] Enter port: ");
 	scanf("%d", &port);
+	printf("[+] Enter username: ");
+	scanf("%s", username);
 
 	server.sin_addr.s_addr = inet_addr(address);
 	server.sin_port = htons(port);
 	server.sin_family = AF_INET;
 
 	if ( connect(socket_fd, (struct sockaddr *)&server, sizeof(server)) < 0) {
-		fprintf(stderr, "[!!] Failed to connect.\n to %s:%d. Error Code: %d\n", address, port, WSAGetLastError());
+		fprintf(stderr, "[ERROR] Failed to connect.\n to %s:%d. Error Code: %d\n", address, port, WSAGetLastError());
 		return -1;
 	}
 
-	printf("[*] Connected to %s:%d\n", address, port);
+	printf("[DEBUG] Connected to %s:%d\n", address, port);
 
+	recv_banner(socket_fd);
+	send_username(socket_fd, username);
 	communicate(socket_fd);
 
 	return 0;
+}
+
+void recv_banner(SOCKET server_fd) {
+	char server_banner[MSG_SIZE] = {0};
+	int recv_size = 0;
+
+	if ((recv_size = recv(server_fd, server_banner, 2000, 0)) == SOCKET_ERROR) {
+		fprintf(stderr, "[ERROR] Failed to receive data.\n Error Code: %d\nPlease Retry.\n", WSAGetLastError());
+		send(server_fd, "QUIT", strlen("QUIT"), 0);
+		exit(-1);
+	} else if (recv_size == 0) {
+		puts("[DEBUG] Connection closed.");
+	}
+	server_banner[recv_size] = '\0';
+	printf("[DEBUG] Received Data.\n%s\n", server_banner);
+}
+
+void send_username(SOCKET server_fd, char *username) {
+	int recv_size = 0;
+	char conf_msg [4] = {0};
+
+	if (send(server_fd, username, strlen(username), 0) == SOCKET_ERROR) {
+		fprintf(stderr, "[ERROR] Could not send username to the server. Error code: %d\n", WSAGetLastError());
+		exit(-1);
+	}
+
+	recv_size = recv(server_fd, conf_msg, sizeof(conf_msg), 0);
+	if (recv_size == 0 || !strcmp(conf_msg, "CONF")) {
+		fprintf(stderr, "[ERROR] Server did not receive username. Aborting.\n");
+		exit(-1);
+	}
 }
 
 void communicate(SOCKET server_fd) {
 	char server_reply[MSG_SIZE], message[MSG_SIZE] = {0};
 
 	int recv_size = 0;
-
-	if ((recv_size = recv(server_fd, server_reply, 2000, 0)) == SOCKET_ERROR) {
-		fprintf(stderr, "[!!] Failed to receive data.\n Error Code: %d\n", WSAGetLastError());
-	} else if (recv_size == 0) {
-		puts("[*] Connection closed.");
-	}
-	server_reply[recv_size] = '\0';
-	printf("[*] Received Data.\n%s\n", server_reply);
 
 	setbuf(stdin, NULL);
 	while (TRUE) {
@@ -79,5 +110,3 @@ void communicate(SOCKET server_fd) {
 		}
 	}
 }
-
-	
--- server.c
+++ server.c
@@ -3,17 +3,20 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
-#define TRUE		1
-#define MSG_SIZE	256
+#define TRUE			1
+#define MSG_SIZE		256
+#define USER_NAME_SIZE	32
 
-void communicate(SOCKET client_socket, char *addr);
-int send_banner(SOCKET client_socket, char *addr);
+void send_banner(SOCKET client_socket, char *addr);
+void recv_username(SOCKET client_socket, char *username);
+void communicate(SOCKET client_socket, char *addr, char *username);
 
 int main(int argc, char *argv[]) {
 	WSADATA	wsa;
 	SOCKET server_socket, client_socket;
 	struct sockaddr_in server, client;
 	const int sockaddr_in_size = sizeof(struct sockaddr_in);
+	char username[USER_NAME_SIZE];
 
 	puts("[*] Initialising Winsock...\n");
 	
@@ -50,7 +53,14 @@ int main(int argc, char *argv[]) {
 	while ( (client_socket = accept(server_socket, (struct sockaddr *)&client, &sockaddr_in_size)) != INVALID_SOCKET) {
 		printf("[*] Accepted connection from: %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
 
-		communicate(client_socket, inet_ntoa(client.sin_addr));
+		/* Send banner to client */
+		send_banner(client_socket, inet_ntoa(client.sin_addr));
+
+		/* Receive user name from client */
+		recv_username(client_socket, username);
+
+		/* Start communication with client */
+		communicate(client_socket, inet_ntoa(client.sin_addr), username);
 	}
 
 	if (client_socket == INVALID_SOCKET) {
@@ -67,29 +77,39 @@ int main(int argc, char *argv[]) {
 
 }
 
-int send_banner(SOCKET client_socket, char *addr) {
+void send_banner(SOCKET client_socket, char *addr) {
 	const char *banner = "Hello, There\nEnter message\nYou can exit by typing \":quit\"\n";
 	if (send(client_socket, banner, strlen(banner), 0) == SOCKET_ERROR) {
 		fprintf(stderr, "[!!] Could not send banner to %s.Error code: %d\n", addr, WSAGetLastError());
-		return -1;
+		exit(-1);
 	}
-	return 0;
 }
 
-void communicate(SOCKET client_socket, char *addr) {
+void recv_username(SOCKET client_socket, char *username) {
+	int recv_size = 0;
+	recv_size = recv(client_socket, username, USER_NAME_SIZE, 0);
+	username[recv_size] = '\0';
+	if (recv_size) {
+		send(client_socket, "CONF", strlen("CONF"), 0);
+		printf("[DEBUG] Received username: %s.\n", username);
+	}
+	else {
+		send(client_socket, "QUIT", strlen("QUIT"), 0);
+		fprintf(stderr, "[!!] Could not receive username %s.Error code: %d\n", WSAGetLastError());
+		exit(-1);
+	}
+}
+
+void communicate(SOCKET client_socket, char *addr, char *username) {
 	int recv_size = 0;
 	char client_reply[MSG_SIZE];
-
-	if (send_banner(client_socket, addr)) {
-		return;
-	}
-
+	
 	while (TRUE) {
 		recv_size = recv(client_socket, client_reply, 2000, 0);
 		client_reply[recv_size] = '\0';
 
 		puts("[DEBUG] Received Message.");
-		printf("%s\n", client_reply);
+		printf("[%s@%s] %s\n", username, addr, client_reply);
 
 		if (!strcmp(client_reply, "QUIT")) {
 			closesocket(client_socket);
